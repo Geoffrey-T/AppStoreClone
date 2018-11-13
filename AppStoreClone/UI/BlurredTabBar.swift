777d7099a8fd0f336a69ba732f275eead52ef1b8
geoffrey thenot <geoffrey.thenot@gmail.com>
2018-11-13 16:35:29 +0100

    Blurred tab bar OK

--- AppStoreClone/UI/BlurredTabBar.swift
+++ AppStoreClone/UI/BlurredTabBar.swift
@@ -12,15 +12,7 @@ class BlurredTabBar: UITabBar {
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-//        isTranslucent = true
-//
-//        let blurredMask = UIVisualEffectView(effect: UIBlurEffect(style: .light))
-//        blurredMask.frame = bounds
-//        blurredMask.autoresizingMask = .flexibleWidth
-//        barTintColor = UIColor.clear
-//        backgroundColor = UIColor.clear
-//
-//        insertSubview(blurredMask, at: 0)
+
         let blurredMask = UIVisualEffectView(effect: UIBlurEffect(style: .light))
         blurredMask.frame = bounds
         blurredMask.autoresizingMask = .flexibleWidth
